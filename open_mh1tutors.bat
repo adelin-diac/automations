f79d6f494b3f9e052646ff2c6d8db16c4aa596f9
adelin-diac <105500899+adelin-diac@users.noreply.github.com>
2023-04-06 21:49:54 +0100

    Initial commit

---
+++ open_mh1tutors.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\adeli\OneDrive\Desktop\MathsHelp_Ireland\Applications\mh1_tutors"
+start "" code .
+
+cd "C:\Users\adeli\OneDrive\Desktop\MathsHelp_Ireland\Applications\mh1_tutors\functions"
+start "" code .

3a2f970c7abc97bdca4f69b09eb35c5badea7c1b
adelin-diac <105500899+adelin-diac@users.noreply.github.com>
2023-05-01 11:53:25 +0100

    update docs

--- excel_stuff/excel_planner.py
+++ excel_stuff/excel_planner.py
@@ -4,8 +4,17 @@ from datetime import datetime, timedelta
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 
-# Function to generate a DataFrame for each month with days as columns and 30-min slots as rows
 def generate_monthly_schedule(year, month):
+    """
+    Generates a DataFrame for a given month with days as columns and 30-min slots as rows.
+    
+    Parameters:
+    year (int): The year for which to generate the schedule.
+    month (int): The month for which to generate the schedule (1 to 12).
+    
+    Returns:
+    pandas.DataFrame: A DataFrame with the daily schedule for the specified month.
+    """
     start_date = datetime(year, month, 1)
     end_date = datetime(year, month + 1, 1) if month != 12 else datetime(year + 1, 1, 1)
     num_days = (end_date - start_date).days
@@ -13,16 +22,24 @@ def generate_monthly_schedule(year, month):
     time_slots = pd.date_range(start_date.replace(hour=6, minute=30), start_date.replace(hour=6, minute=0) + timedelta(days=1), freq='30min').strftime('%H:%M')
     
     def day_suffix(day):
+        """
+        Returns the appropriate ordinal suffix for a given day.
+        
+        Parameters:
+        day (int): The day for which to return the ordinal suffix (1 to 31).
+        
+        Returns:
+        str: The ordinal suffix for the given day.
+        """
         if 4 <= day <= 20 or 24 <= day <= 30:
             return "th"
         else:
             return ["st", "nd", "rd"][day % 10 - 1]
     
-    column_labels = [start_date.replace(day=i).strftime('%A %d') + day_suffix(i) for i in range(1, num_days + 1)]
+    column_labels = [start_date.replace(day=i).strftime('%A %-d') + day_suffix(i) for i in range(1, num_days + 1)]
     df = pd.DataFrame(index=time_slots, columns=column_labels)
 
     return df
-
 
 # Create a new Excel workbook and add a worksheet for each month
 year = 2023

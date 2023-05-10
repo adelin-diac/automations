b77cff75c7eb9481e1acd269c0cb835e384ad933
adelin-diac <adelin334@gmail.com>
2023-05-11 00:08:47 +0100

    Update react_frontend_with_firebase_functions.bat

--- server_starts/react_frontend_with_firebase_functions.bat
+++ server_starts/react_frontend_with_firebase_functions.bat
@@ -1,9 +1,9 @@
 @REM This script assumes the functions folder is inside project directory
 
-set create_react_app_frontend="C:\Users\adeli\OneDrive\Desktop\MathsHelp_Ireland\Applications\mh1_tutors"
-set firebase_functions="C:\Users\adeli\OneDrive\Desktop\MathsHelp_Ireland\Applications\mh1_tutors\functions"
+set create_react_app_frontend="C:\Users\adeli\Desktop\mh1_tutors\Applications\mh1_tutors"
+set firebase_functions="C:\Users\adeli\Desktop\mh1_tutors\Applications\mh1_tutors\functions"
 
 
 START "React Frontend" cmd /k "cd %create_react_app_frontend% & code . & npm start"
 
-START "Firebase Functions" cmd /k "cd %firebase_functions% & code . & cd ..\ & firebase emulators:start --only functions"
+START "Firebase Functions" cmd /k "cd %create_react_app_frontend% & firebase emulators:start --only functions"

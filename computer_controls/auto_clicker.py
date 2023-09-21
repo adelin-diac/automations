4f054899467affbe41a7a9c080d70d95463d19d9
adelin-diac <adelin334@gmail.com>
2023-09-21 16:00:22 +0100

    Update auto_clicker.py

--- computer_controls/auto_clicker.py
+++ computer_controls/auto_clicker.py
@@ -7,7 +7,7 @@ DELAY_BETWEEN_CLICKS = 0.01 # In seconds
 
 def main():
     is_running = False
-    print("Script started. Press 'q' to stop/start the clicking.")
+    print("Script started. Press 'c' to stop/start the clicking.")
 
     try:
         while True:
@@ -15,7 +15,7 @@ def main():
                 pyautogui.click()
                 time.sleep(DELAY_BETWEEN_CLICKS)
             
-            if keyboard.is_pressed('q'):
+            if keyboard.is_pressed('c'):
                 is_running = not is_running
                 if is_running:
                     print("Clicking started.")
